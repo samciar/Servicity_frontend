5efefcfe13a4c67616bc7131e5cda820214e9d4d
Samuel Cifuentes <sam.cifuentes93@gmail.com>
2025-05-05 20:39:14 -0500

    Landing page creada

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../public/css/styles.css'
+import { templates } from './templates.js'
+import $ from 'jquery';
+        window.$ = $;
+        window.jQuery = $;
+
+templates()
---
+++ src/templates.js
@@ -0,0 +1,4 @@
+export function templates() {
+    console.log('Cargar plantilla');
+    
+}
